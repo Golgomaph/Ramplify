28043fc36b9a7e2e96f47b02d5d9018d5e0e73c4
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-02-15 21:01:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,32 +22,14 @@ echo "Update OS?"
 read REPLYUPDATE
 
 echo
-echo "Choosing functions"
-echo "---------------------------------------------"
-echo "Do you want to..."
-echo -n "  ...stream audio via bluetooth (BlueALSA)? [y/N] "
-read REPLYBLUETOOTH
-echo -n "  ...stream audio via WiFi-UPnP (gmrender-resurrect)? [y/N] "
-read REPLYUPNP
-echo -n "  ...stream audio via Apple AirPlay (shairport-sync)? [y/N] "
-read REPLYSHAIRPORT
-echo -n "  ...see RasPi as streaming device in Spotify (Raspotify)? [y/N] "
-read REPLYSPOTIFY
-echo -n "  ...use multi-room functionality using more then one RasPi (snapclient})? [y/N] "
-read REPLYSNAPCAST
-echo -n "  ...generate wifi access-point if no known wifi is available? [y/N] "
-read REPLYAUTOAP
-
-echo
 echo "Installing components"
 echo "---------------------------------------------"
 if [[ "$REPLYUPDATE" =~ ^(yes|y|Y)$ ]]; then sudo apt-get install update; fi;
 if [[ "$REPLYUPDATE" =~ ^(yes|y|Y)$ ]]; then sudo apt-get install upgrade; fi;
-if [[ "$REPLYBLUETOOTH" =~ ^(yes|y|Y)$ ]]; then sudo ./install-bluetooth.sh; fi;
-if [[ "$REPLYUPNP" =~ ^(yes|y|Y)$ ]]; then sudo ./install-upnp.sh; fi;
-if [[ "$REPLYSHAIRPORT" =~ ^(yes|y|Y)$ ]]; then sudo ./install-shairport.sh; fi;
-if [[ "$REPLYSPOTIFY" =~ ^(yes|y|Y)$ ]]; then sudo ./install-spotify.sh; fi;
-if [[ "$REPLYSNAPCAST" =~ ^(yes|y|Y)$ ]]; then sudo ./install-snapcast-client.sh; fi;
-if [[ "$REPLYAUTOAP" =~ ^(yes|y|Y)$ ]]; then yes | ./install-autoap.sh; fi;
 
-sudo ./rebootafterfinish.sh
+echo
+echo -n "----------------------------------------------------------------"
+echo -n "Installation finished. Reboot now? [y/N] "
+read REPLY
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+sudo reboot
