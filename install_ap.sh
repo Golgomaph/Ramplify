a5f7f78427e4a4abf518aced852a93c6d60181ba
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-02-15 22:16:33 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -1,1 +1,6 @@
-echo "Peter"
+echo "Installing ap"
+apt-get install hostapd wpasupplicant dnsmasq
+update-rc.d -f hostapd remove
+update-rc.d -f dnsmasq remove
+cat ./templates/interface.template >> ~/etc/network/interfaces
+
