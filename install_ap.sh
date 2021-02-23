8f76bb159921931f18a959582cfcede144c1256f
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 14:06:29 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -4,3 +4,6 @@ update-rc.d -f hostapd remove
 update-rc.d -f dnsmasq remove
 cat ./templates/interface.template >> ~/etc/network/interfaces
 
+sudo raspi-config nonint do_boot_wait 0
+cat ./templates/hostapd.template >> ~/etc/default/hostapd
+cat ./templates/hostapd.conf.template >> ~/etc/hostapd/hostapd/hostapd.conf
