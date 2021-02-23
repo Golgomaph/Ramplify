bcc4e8289c3d71e1b6d7b41843e5fe2925f23775
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 22:15:53 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -9,13 +9,11 @@ cat ./templates/interfaces.template >> /etc/network/interface
 sudo raspi-config nonint do_boot_wait 0
 
 cat ./templates/hostapd.template >> /etc/default/hostapd
-ifdown wlan0
+
 
 cat ./templates/hostapd.conf.template >> /etc/hostapd/hostapd.conf
 hostapd -d /etc/hostapd/hostapd.conf
 
-ifdown wlan0
 service hostapd start
 ifconfig wlan0 192.168.40.1
 service dnsmasq start
-tail -f /var/log/syslog
