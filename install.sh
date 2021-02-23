2e4bcb9b13b7ed77ce8dd6a9dd7bccc4ce90e2f5
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 18:13:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,30 @@ echo "Select new passwort"
 echo "---------------------------------------------"
 sudo passwd pi;
 
-echo "Choose hostname"
+echo "Hostname is Ramplify"
 echo "---------------------------------------------"
-read -p "Hostname [$(hostname)]: " HOSTNAME
+#read -p "Hostname [$(hostname)]: " HOSTNAME
+export HOSTNAME="Ramplify"
 sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
 
 CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+#read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+CURRENT_PRETTY_HOSTNAME="Ramplify"
 sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+
 echo "192.168.40.1 simple.audio" >> /etc/hosts
 echo "set" > /etc/passcheck;
 fi;
 
-echo -n "Update raspian? [y/n] >> "
-read REPLYUPDATE
-if [[ "$REPLYUPDATE" =~ ^(yes|y|Y)$ ]]; then sudo apt-get install update; fi;
-if [[ "$REPLYUPDATE" =~ ^(yes|y|Y)$ ]]; then sudo apt-get install upgrade; fi;
-sudo ./install_ap.sh
+echo "Update raspian"
+echo "---------------------------------------------"
+sudo apt-get install update
+
+echo "Upgrade raspian"
+echo "---------------------------------------------"
+sudo apt-get install upgrade
+
+#sudo ./install_ap.sh
 echo -n "Installation finished. You may reboot now.. Reboot? >> [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
