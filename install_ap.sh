554b04521585e0a2ce13a18e4d91308d3fca75e4
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 20:06:08 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -1,26 +1,15 @@
 #!/bin/bash -e
 
-echo "---------------------------------------------"
-echo "Installing ap"
 apt-get install hostapd wpasupplicant dnsmasq -y
 update-rc.d -f hostapd remove
 update-rc.d -f dnsmasq remove
 
-echo "---------------------------------------------"
 cat ./templates/interfaces.template >> /etc/network/interfaces
-echo "Interface changed"
 
-echo "---------------------------------------------"
 sudo raspi-config nonint do_boot_wait 0
-echo "Setting the boot option wait for network"
 
-echo "---------------------------------------------"
 cat ./templates/hostapd.template >> /etc/default/hostapd
-echo "Hostapd changed"
 
-echo "---------------------------------------------"
 cat ./templates/hostapd.conf.template >> /etc/hostapd/hostapd.conf
-echo "Hostapd.conf changed"
 
-echo "---------------------------------------------"
-echo "Installing finished"
+
