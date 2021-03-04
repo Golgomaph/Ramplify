d4b195ccbfc208572420d192a9d5d5dfe5dec9f4
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-03-04 18:09:36 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -4,6 +4,9 @@ apt-get install hostapd wpasupplicant dnsmasq -y
 update-rc.d -f hostapd remove
 update-rc.d -f dnsmasq remove
 
+#Give HDMI AND AUX PERMISSIONS
+sudo chmod a+w ~/.asoundrc
+
 cat ./templates/interfaces.template >> /etc/network/interface
 
 iface test_network inet dhcp
