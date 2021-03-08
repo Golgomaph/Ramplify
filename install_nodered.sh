9a9b19113c7137fa9d6728a5a6bab3f76e3d4e17
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-03-08 22:56:12 +0100

    Update install_nodered.sh

--- install_nodered.sh
+++ install_nodered.sh
@@ -3,3 +3,6 @@
 yes | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
 sudo systemctl enable nodered.service
+
+sudo rm /home/pi/.node-red/settings.js
+sudo cp /home/pi/ramplify/templates/settings.js.template /home/pi/.node-red/
