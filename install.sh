eafd524ad6739417205fb2edc1c07935e1a407c3
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-03-08 21:07:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ echo "Installing Access Point finished"
 
 echo "Installing NodeRed"
 echo "---------------------------------------------"
-./install_nodered.sh;
+./install_nodered.sh -y
 echo "Installing NodeRed finished"
 
 echo -n "Installation finished. You may reboot now.. Reboot? >> [y/N] "
