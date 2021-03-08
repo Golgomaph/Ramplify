e0b3d9b88687408764fd3954fd532f1676169e45
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-03-08 21:18:33 +0100

    Update install_nodered.sh

--- install_nodered.sh
+++ install_nodered.sh
@@ -1,1 +1,3 @@
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+#!/bin/bash -e
+
+yes | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
