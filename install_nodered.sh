3eb461c1ceec4d26777a1cc054f75dbd5c1386da
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-03-09 21:35:20 +0100

    Update install_nodered.sh

--- install_nodered.sh
+++ install_nodered.sh
@@ -9,3 +9,13 @@ sudo node-red-stop
 sudo rm /home/pi/.node-red/settings.js
 sudo cp /home/pi/Ramplify/templates/settings.js.template /home/pi/.node-red/
 sudo mv /home/pi/.node-red/settings.js.template /home/pi/.node-red/settings.js
+
+sudo apt install iptables-persistent -y
+sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 1880
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
+sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1880
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
