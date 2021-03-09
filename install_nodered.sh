cfed4026291439662c2d259600eb7d624d7c79d7
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-03-09 22:20:32 +0100

    Update install_nodered.sh

--- install_nodered.sh
+++ install_nodered.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-yes | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-
 sudo systemctl enable nodered.service
 
 sudo node-red-stop
