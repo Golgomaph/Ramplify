d43e75d45400c6fc9fea03a0c045e542a8b6a071
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-04-14 18:27:36 +0200

    Delete shScripts directory

--- shScripts/shairportend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-bluetoothctl << EOT
-discoverable on
-EOT
-amixer cset numid=3 90%
-exit 0
--- shScripts/shairportstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-bluetoothctl << EOT
-discoverable off
-EOT
-amixer cset numid=3 90%
-exit 0
